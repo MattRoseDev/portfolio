bf958a05058305ac6df855a25d15d12faaae182a
Mahdi <mahdineer@pm.me>
2021-12-10 01:47:26 +0330

    feat: add development tools

---
+++ src/features/Home/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./Home.module.css";
+
+export function Home() {
+  return (
+    <div className={styles.wrapper}>
+      <div>
+        <h1 className={styles.title}>Hey, I&apos;m Mahdi</h1>
+      </div>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
+  mode: "jit",
+  purge: [
+    "./public/**/*.html",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      primary: colors.blue,
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      red: colors.rose,
+      green: colors.green,
+      yellow: colors.amber,
+      blue: colors.blue,
+    },
   },
   variants: {
     extend: {},
