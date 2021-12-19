d46aa2cd3dbc0e27e4e5c51c6eef321e80fac470
Matt Rose <mattroseinbox@gmail.com>
2021-12-19 16:19:07 +0330

    perf: improve Section components

--- src/components/Section/Description/index.tsx
+++ src/components/Section/Description/index.tsx
@@ -1,7 +1,9 @@
+import classNames from "classnames";
 import { DescriptionProps } from "../@types";
 import styles from "../Section.module.css";
 
 export function Description(props: DescriptionProps) {
-  const { children } = props;
-  return <p className={styles.description}>{children}</p>;
+  const { children, className } = props;
+  const descriptionStyles = classNames(styles.description, className);
+  return <p className={descriptionStyles}>{children}</p>;
 }
