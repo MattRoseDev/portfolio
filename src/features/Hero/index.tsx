e5b40483693ecc872ad5c562f856d3070783119e
Matt Rose <mattroseinbox@gmail.com>
2021-12-21 22:47:23 +0330

    perf: implement About feature

--- src/features/Hero/index.tsx
+++ src/features/Hero/index.tsx
@@ -7,7 +7,7 @@ export function Hero() {
         <span>Hey, </span>
         <span> I&apos;m Mahdi.</span>
       </h2>
-      <h3 className={styles.bigHeading}>Brings ideas to life with code.</h3>
+      <h3 className={styles.smallHeading}>Brings ideas to life with code.</h3>
       <p className={styles.description}>
         Full Stack Developer with <strong>+5 years</strong> of experience
         developing complicated applications. Specializes in React, Node.js and
