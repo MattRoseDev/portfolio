73c8788321dd69d1878f906b2ea6450c71d3dfa8
Mahdi <mahdineer@pm.me>
2021-12-21 22:47:23 +0330

    perf: implement About feature

--- src/features/Home/index.tsx
+++ src/features/Home/index.tsx
@@ -1,11 +1,13 @@
 import styles from "./Home.module.css";
 import { SocialList } from "@components/SocialList";
+import { About } from "@features/About";
 import { Hero } from "@features/Hero";
 
 export function Home() {
   return (
     <div className={styles.wrapper}>
       <Hero />
+      <About />
       <SocialList />
     </div>
   );
