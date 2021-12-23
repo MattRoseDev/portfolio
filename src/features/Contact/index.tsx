5fb73069b857b7cd268f1d6a88abc3f429ce7718
Mahdi <favecode@pm.me>
2021-12-24 00:28:07 +0330

    perf: improve Contact feature

--- src/features/Contact/index.tsx
+++ src/features/Contact/index.tsx
@@ -8,9 +8,9 @@ export function Contact() {
     <Section id="contact" className={styles.contact}>
       <div className={styles.title}>Get In Touch</div>
       <Description className={styles.description}>
-        I&apos;m currently looking for any new opportunities, my inbox is always
-        open. Whether you have a question or just want to say hi, I&apos;ll try
-        my best to get back to you!
+        Currently, I&apos;m looking for any new opportunities, my inbox is
+        always open. Whether you have a question or just want to say hi,
+        I&apos;ll try my best to get back to you!
       </Description>
       <LinkButton className={styles.email} href={email}>
         Say Hello
