8000f747b54a8c2d61ed6d1d26a55b4e80e5b5e2
Mahdi <mahdineer@pm.me>
2021-12-31 21:34:37 +0330

    feat: implement storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,14 @@
+import "../src/styles/globals.css";
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  darkMode: {
+    current: "light",
+  },
+};
