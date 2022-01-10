6d7480d8457326c76bcf71aaf146db45038b2858
Mahdi <favecode@pm.me>
2022-01-10 11:32:51 +0300

    perf: imporve Section component

--- src/components/Section/Section/index.tsx
+++ src/components/Section/Section/index.tsx
@@ -1,10 +1,14 @@
+import classNames from "classnames";
 import { SectionProps } from "../@types";
 import styles from "../Section.module.css";
 
 export function Section(props: SectionProps) {
   const { children, ...others } = props;
   return (
-    <section className={styles.section} {...others}>
+    <section
+      {...others}
+      className={classNames(styles.section, others.className)}
+    >
       {children}
     </section>
   );
