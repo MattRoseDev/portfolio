f95ca6188457d0e16d2cc669a1130b37c902cb7e
Matt Rose <mattroseinbox@gmail.com>
2022-01-13 02:03:20 +0300

    feat: implement Experience feature

---
+++ src/features/Experience/@types/index.ts
@@ -0,0 +1,7 @@
+export type Job = {
+  company: string;
+  role: string;
+  date: string;
+  descriptions: string[];
+  techStack: string[];
+};
--- src/features/Home/index.tsx
+++ src/features/Home/index.tsx
@@ -2,6 +2,7 @@ import styles from "./Home.module.css";
 import { SocialList } from "@components/SocialList";
 import { About } from "@features/About";
 import { Contact } from "@features/Contact";
+import { Experience } from "@features/Experience";
 import { Hero } from "@features/Hero";
 
 export function Home() {
@@ -9,6 +10,7 @@ export function Home() {
     <div className={styles.wrapper}>
       <Hero />
       <About />
+      <Experience />
       <Contact />
       <SocialList />
     </div>
