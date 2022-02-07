b0c7d1280a09646eb11ead471beb2004f0563674
Mahdi <mahdineer@pm.me>
2022-02-08 02:56:56 +0300

    perf: improve Hero feature

--- src/features/Hero/index.tsx
+++ src/features/Hero/index.tsx
@@ -4,14 +4,15 @@ export function Hero() {
   return (
     <section className={styles.hero}>
       <h2 className={`${styles.bigHeading} ${styles.highlight}`}>
-        <span>Hey, </span>
-        <span> I&apos;m Mahdi.</span>
+        <span>Hi, </span>
+        <span>I&apos;m Mahdi.</span>
       </h2>
       <h3 className={styles.smallHeading}>Brings ideas to life with code.</h3>
       <p className={styles.description}>
         Full Stack Developer with <strong>+5 years</strong> of experience
-        developing complicated applications. Specializes in React, Node.js and
-        Golang.
+        developing complicated applications.
+        <br />
+        <div>Specializes in React, Node.js and Golang</div>
       </p>
     </section>
   );
