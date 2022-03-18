c02c63abfd86c81a95b6413561d3590c664891b4
Mahdi <mahdineer@pm.me>
2022-03-18 23:49:55 +0300

    perf: improve Header component for desktop layout

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -4,10 +4,10 @@ import { navigationItems } from "./config";
 
 export function Navigation() {
   return (
-    <div className={styles.links}>
+    <div className={styles.navigation}>
       {navigationItems.map(({ href, title }) => (
         <Link href={href} key={title}>
-          <a className={styles.navItem}>{title}</a>
+          <a className={styles.item}>{title}</a>
         </Link>
       ))}
     </div>
