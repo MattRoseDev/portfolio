4c61246bec0a9c2868b3c7783c3a5bfb11769c33
Mahdi <mahdineer@pm.me>
2022-03-20 11:41:51 +0300

    refactor: improve Experience feature

--- src/components/Icon/index.ts
+++ src/components/Icon/index.ts
@@ -1,42 +1,18 @@
 import { createElement, SVGAttributes } from "react";
-
-import CameraFill from "./svg/camera-fill.svg";
 import Camera from "./svg/camera.svg";
-import ChevronRightFill from "./svg/chevron-right-fill.svg";
 import ChevronRight from "./svg/chevron-right.svg";
 import Codepen from "./svg/codepen.svg";
-import ExternalLinkFill from "./svg/external-link-fill.svg";
 import ExternalLink from "./svg/external-link.svg";
-import GithubFill from "./svg/github-fill.svg";
 import Github from "./svg/github.svg";
-import LinkedinFill from "./svg/linkedin-fill.svg";
 import Linkedin from "./svg/linkedin.svg";
 
 export const iconsList = {
-  camera: {
-    outline: Camera,
-    fill: CameraFill,
-  },
-  chevronRight: {
-    outline: ChevronRight,
-    fill: ChevronRightFill,
-  },
-  codepen: {
-    outline: Codepen,
-    fill: Codepen,
-  },
-  externalLink: {
-    outline: ExternalLink,
-    fill: ExternalLinkFill,
-  },
-  github: {
-    outline: Github,
-    fill: GithubFill,
-  },
-  linkedin: {
-    outline: Linkedin,
-    fill: LinkedinFill,
-  },
+  Camera,
+  ChevronRight,
+  Codepen,
+  ExternalLink,
+  Github,
+  Linkedin,
 };
 
 export type IconNames = keyof typeof iconsList;
@@ -44,11 +20,10 @@ export type IconNames = keyof typeof iconsList;
 export interface IconProps extends SVGAttributes<SVGElement> {
   name: IconNames;
   className?: string;
-  type?: "outline" | "fill";
 }
 
-export function Icon({ name, className, type, ...props }: IconProps) {
-  const icon = iconsList[name][type || "outline"];
+export function Icon({ name, className, ...props }: IconProps) {
+  const icon = iconsList[name];
 
   if (!icon) {
     throw Error(`"${name}" is not a valid icon name`);
