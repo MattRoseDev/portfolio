b1f0e062cfea5dd7117db519f34fc6e09b4276f5
Mahdi <mahdineer@pm.me>
2022-03-26 13:41:12 +0300

    feat: add SocialList to Home feature

---
+++ src/components/SocialList/@types/index.ts
@@ -0,0 +1,3 @@
+export type SocialListProps = {
+  className?: string;
+};
--- src/components/SocialList/index.tsx
+++ src/components/SocialList/index.tsx
@@ -1,11 +1,16 @@
+import classNames from "classnames";
 import Link from "next/link";
 import styles from "./SocialList.module.css";
 import { Icon } from "src/components/Icon";
 import { SOCIAL_LIST } from "src/constants";
+import type { SocialListProps } from "./@types";
 
-export function SocialList() {
+export function SocialList(props: SocialListProps) {
+  const { className } = props;
+  const socialListClasses = classNames(styles.socialList, className);
+
   return (
-    <ul className={styles.socialList}>
+    <ul className={socialListClasses}>
       {SOCIAL_LIST.map(({ title, href, icon }) => (
         <li key={title}>
           <Link href={href}>
