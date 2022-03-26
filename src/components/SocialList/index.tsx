1b0c1563e3e34860302178d1447c0b942fac7212
Mahdi <mahdineer@pm.me>
2022-03-26 03:49:29 +0300

    perf: move SOCIAL_LIST to constant directory

--- src/components/SocialList/index.tsx
+++ src/components/SocialList/index.tsx
@@ -1,38 +1,16 @@
 import Link from "next/link";
 import styles from "./SocialList.module.css";
 import { Icon } from "src/components/Icon";
+import { SOCIAL_LIST } from "src/constants";
 
 export function SocialList() {
-  const socialList = [
-    {
-      title: "Github",
-      icon: <Icon name="Github" className="w-6" />,
-      href: "https://github.com/mahdineer",
-    },
-    {
-      title: "Linkedin",
-      icon: <Icon name="Linkedin" className="w-6" />,
-      href: "https://www.linkedin.com/in/mahdineer",
-    },
-    {
-      title: "Unsplash",
-      icon: <Icon name="Camera" className="w-6" />,
-      href: "https://unsplash.com/@mahdineer",
-    },
-    {
-      title: "Codepen",
-      icon: <Icon name="Codepen" className="w-6" />,
-      href: "https://codepen.io/mahdineer",
-    },
-  ];
-
   return (
     <ul className={styles.socialList}>
-      {socialList.map(({ title, href, icon }) => (
+      {SOCIAL_LIST.map(({ title, href, icon }) => (
         <li key={title}>
           <Link href={href}>
             <a className={styles.socialItem} target="_blank">
-              {icon}
+              {<Icon name={icon} className="w-6" />}
             </a>
           </Link>
         </li>
