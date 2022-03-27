51639207ffb6c2a97ad84f9eb1ad48254bf22969
Mahdi <mahdineer@pm.me>
2022-03-27 23:10:31 +0300

    feat: add meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,6 +13,43 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;700&&display=swap"
             rel="stylesheet"
           />
+          <meta
+            name="description"
+            content=" Frontend Developer with +4 years of experience developing web applications."
+          />
+          {/* Google / Search Engine Tags */}
+          <meta itemProp="name" content="Mahdi" />
+          <meta
+            itemProp="description"
+            content=" Frontend Developer with +4 years of experience developing web applications."
+          />
+          <meta
+            itemProp="image"
+            content="https://mahdi.codes/images/avatars/mahdi.jpg"
+          />
+          {/* Facebook Meta Tags */}
+          <meta property="og:url" content="https://mahdi.codes" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Mahdi" />
+          <meta
+            property="og:description"
+            content=" Frontend Developer with +4 years of experience developing web applications."
+          />
+          <meta
+            property="og:image"
+            content="https://mahdi.codes/images/avatars/mahdi.jpg"
+          />
+          {/* Twitter Meta Tags */}
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content="Mahdi" />
+          <meta
+            name="twitter:description"
+            content=" Frontend Developer with +4 years of experience developing web applications."
+          />
+          <meta
+            name="twitter:image"
+            content="https://mahdi.codes/images/avatars/mahdi.jpg"
+          />
         </Head>
         <body>
           <Main />
