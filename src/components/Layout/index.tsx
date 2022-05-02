19535c8119d7f425656985970ba10c488a629dfa
Mahdi <mahdineer@pm.me>
2022-05-02 19:12:55 +0300

    chore: update packages

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,9 +1,19 @@
+import { useEffect, useState } from "react";
 import { LayoutProps } from "./@types";
 import styles from "./Layout.module.css";
 import { Header } from "@components/Header";
 
 export function Layout(props: LayoutProps) {
   const { children } = props;
+  const [hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  if (!hasMounted) {
+    return null;
+  }
 
   return (
     <div className={styles.wrapper}>
