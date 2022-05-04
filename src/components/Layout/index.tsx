1e17335355c7869a3edd506028d33f04662daa2e
Matt Rose <mattroseinbox@gmail.com>
2022-05-04 20:30:33 +0300

    Revert "chore: update packages"
    This reverts commit 19535c8119d7f425656985970ba10c488a629dfa.

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,19 +1,9 @@
-import { useEffect, useState } from "react";
 import { LayoutProps } from "./@types";
 import styles from "./Layout.module.css";
 import { Header } from "@components/Header";
 
 export function Layout(props: LayoutProps) {
   const { children } = props;
-  const [hasMounted, setHasMounted] = useState(false);
-
-  useEffect(() => {
-    setHasMounted(true);
-  }, []);
-
-  if (!hasMounted) {
-    return null;
-  }
 
   return (
     <div className={styles.wrapper}>
