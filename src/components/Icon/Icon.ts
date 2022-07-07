133928233681a5b56c444fa91c3aa487ff39c0e3
Matt Rose <mattroseinbox@gmail.com>
2022-07-07 21:57:25 +0300

    perf: improve Icon component

---
+++ src/components/Icon/Icon.ts
@@ -0,0 +1,37 @@
+import { createElement, SVGAttributes } from "react";
+import ChevronRight from "./svg/chevron-right.svg";
+import Codepen from "./svg/codepen.svg";
+import ExternalLink from "./svg/external-link.svg";
+import Github from "./svg/github.svg";
+import Linkedin from "./svg/linkedin.svg";
+import Unsplash from "./svg/unsplash.svg";
+
+export const iconsList = {
+  Unsplash,
+  ChevronRight,
+  Codepen,
+  ExternalLink,
+  Github,
+  Linkedin,
+};
+
+export type IconNames = keyof typeof iconsList;
+
+export interface IconProps extends SVGAttributes<SVGElement> {
+  name: IconNames;
+  className?: string;
+}
+
+export function Icon({ name, className, ...props }: IconProps) {
+  const icon = iconsList[name];
+
+  if (!icon) {
+    throw Error(`"${name}" is not a valid icon name`);
+  }
+
+  return createElement(icon, {
+    className,
+    role: "icon",
+    ...props,
+  });
+}
