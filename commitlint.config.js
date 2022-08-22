9a1c8235d50d810d282b5270ae07a7b0439046cd
Mahdi <mahdineer@pm.me>
2022-08-22 21:30:53 +0200

    chore: improve commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,14 +1,3 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const configConventional = require("@commitlint/config-conventional");
-
 module.exports = {
   extends: ["@commitlint/config-conventional"],
-  rules: {
-    ...configConventional.rules,
-    "type-enum": [
-      2,
-      "always",
-      ["hotfix", ...configConventional.rules["type-enum"][2]],
-    ],
-  },
 };
