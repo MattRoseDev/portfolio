9c867435967cc93fcb8d9a85b61fc6bc3c222cc1
Mahdi <mahdineer@pm.me>
2022-10-18 22:22:46 +0200

    feat: implement NotFound page

---
+++ pages/404.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import { NotFound } from "@features/NotFound";
+
+const NotFoundPage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Page Not Found</title>
+      </Head>
+      <NotFound />
+    </>
+  );
+};
+
+export default NotFoundPage;
