807835d29a2992b5770355ff83056988a2a03ba0
Matt Rose <mattroseinbox@gmail.com>
2023-05-22 21:40:53 +0200

    refactor(Experience): improve styles

--- src/features/Experience/Experience.tsx
+++ src/features/Experience/Experience.tsx
@@ -1,91 +1,72 @@
 import classNames from "classnames";
 import { v4 as uuid } from "uuid";
-import { Tab, Transition } from "@headlessui/react";
 import styles from "./Experience.module.css";
 import { Section, Title } from "@/components/Section";
 import { JOBS } from "@/constants";
 import { formatJobDate } from "@/utils";
 import isEmpty from "lodash/isEmpty";
+import size from "lodash/size";
 
 export function Experience() {
   return (
     <Section id="experience" className={styles.wrapper}>
-      <Title>Where I&apos;ve worked</Title>
+      <Title className="ml-2">Work experience</Title>
 
-      <Tab.Group>
-        <div className={styles.tabGroup}>
-          <Tab.List className={styles.tabList}>
-            {JOBS.map(({ company, role }) => (
-              <Tab
-                key={uuid()}
-                className={({ selected }) =>
-                  selected
-                    ? classNames(styles.tabWrapper, styles.selected)
-                    : classNames(styles.tabWrapper)
-                }
-              >
-                <span className={styles.tab}>
-                  <span>{company}</span>
-                  <span className={styles.role}>{role}</span>
-                </span>
-              </Tab>
-            ))}
-          </Tab.List>
-          <Tab.Panels>
-            {JOBS.map(
-              ({ company, role, labels, dates, descriptions, techStack }) => (
-                <Tab.Panel
-                  key={`${company}-${role}`}
-                  className={styles.tabPanel}
-                >
-                  {({ selected }) => (
-                    <Transition
-                      show={selected}
-                      enter={styles.enterTransition}
-                      enterFrom={styles.enterFromTransition}
-                      enterTo={styles.enterToTransition}
-                    >
-                      <div className={styles.head}>
-                        <div className={styles.role}>{role}</div>
-                        <div className={styles.labels}>
-                          {!!labels?.length &&
-                            labels.map(label => (
-                              <div key={label} className={styles.label}>
-                                {label}
-                              </div>
-                            ))}
+      <div className={styles.experience}>
+        {JOBS.map(
+          (
+            { company, role, labels, dates, descriptions, techStack },
+            index,
+          ) => (
+            <div key={`${company}-${role}`} className={styles.section}>
+              <div>
+                {size(JOBS) - 1 > index && <div className={styles.line} />}
+                <div className={styles.circle}>
+                  <div className={classNames(styles.dot)} />
+                  {!index && <div className={classNames(styles.ping)} />}
+                </div>
+              </div>
+
+              <div className={styles.job}>
+                <div className={styles.head}>
+                  <span>
+                    <div className={styles.role}>{role}</div>
+                    <div className={styles.company}>{company}</div>
+                  </span>
+                  <div className={styles.labels}>
+                    {!!labels?.length &&
+                      labels.map(label => (
+                        <div key={label} className={styles.label}>
+                          {label}
                         </div>
-                        <small className={styles.date}>
-                          {formatJobDate(dates)}
-                        </small>
-                      </div>
-                      {!isEmpty(descriptions) && (
-                        <div className={styles.descriptions}>
-                          {descriptions.map(description => (
-                            <div
-                              key={uuid()}
-                              className={styles.description}
-                              dangerouslySetInnerHTML={{ __html: description }}
-                            />
-                          ))}
-                        </div>
-                      )}
-                      <div className={styles.techStack}>
-                        <div>Tech Stack: </div>
-                        {techStack.map(t => (
-                          <div key={uuid()} className={styles.item}>
-                            {t}
-                          </div>
-                        ))}
-                      </div>
-                    </Transition>
-                  )}
-                </Tab.Panel>
-              ),
-            )}
-          </Tab.Panels>
-        </div>
-      </Tab.Group>
+                      ))}
+                  </div>
+                  <small className={styles.date}>{formatJobDate(dates)}</small>
+                </div>
+                {!isEmpty(descriptions) && (
+                  <div className={styles.descriptions}>
+                    {descriptions.map(description => (
+                      <div
+                        key={uuid()}
+                        className={styles.description}
+                        dangerouslySetInnerHTML={{ __html: description }}
+                      />
+                    ))}
+                  </div>
+                )}
+                <div className={styles.techStack}>
+                  <div>Tech Stack: </div>
+                  {techStack.map(t => (
+                    <div key={uuid()} className={styles.item}>
+                      {t}
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          ),
+        )}
+      </div>
     </Section>
   );
 }
